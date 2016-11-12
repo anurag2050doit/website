628bb806e71c202968cf39d797ab85f3aebc6b0f
Anurag Misra <Anurag Misra>
2016-11-12 11:39:49 +0530

    Redirect

--- website/urls.py
+++ website/urls.py
@@ -3,9 +3,10 @@ from django.conf.urls import include , url
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-
+from . import views
 
 urlpatterns = [
+	url(r'^$', views.index, name='index'),
     url(r'^admin/', admin.site.urls),
     url(r'^music/', include('music.urls')),
 ]
